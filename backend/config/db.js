c7516af2d804aef3cc47175b90b8cd3e9239c245
Haiderj01 <haiderjhalod05l@gmail.com>
2026-03-24 10:31:17 +0530

    removed config

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://jhalodhaider_db_user:UnmDFcRfyjuyoID2@cluster0.ib1lw65.mongodb.net/L-M-S')
-    .then(() => {console.log('Connected to MongoDB')})
-};
