6079a9995827a9f1541aac3d7bfc9406fcea53b3
Haiderj01 <haiderjhalod05l@gmail.com>
2026-08-15 18:02:08 +0530

    Test CORS

--- backend/server.js
+++ backend/server.js
@@ -11,16 +11,11 @@ dotenv.config();
 
 
 const app = express();
-const port = 4000;
-
+const port = process.env.PORT || 4000;
 // middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      process.env.FRONTEND_URL,
-    ],
+    origin: true,
     credentials: true,
   })
 );
