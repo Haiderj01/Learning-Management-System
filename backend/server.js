bcce2e89467e1416d0df29f1c98650c31f36296a
Haiderj01 <haiderjhalod05l@gmail.com>
2026-08-15 18:16:50 +0530

    Add logging middleware to track request methods and URLs

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ app.use(
   })
 );
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 
 app.use(clerkMiddleware());
